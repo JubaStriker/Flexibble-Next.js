09e201471bbbfd4eea1868b5acda7f8914e92a62
JubaStriker <jubastriker@gmail.com>
2023-06-27 01:06:46 +0600

    Necessary files added

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <section className="paddings flex-col flex-start mb-16">
+            <h1>Categories</h1>
+            <h1>Posts</h1>
+            <h1>Load more</h1>
+        </section>
+    );
+};
+
+export default Home;
