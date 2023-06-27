d244018ca303d22522f8c2983f8caa641ab7bfda
JubaStriker <jubastriker@gmail.com>
2023-06-27 13:26:02 +0600

    Navbar and Footer completed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { NavLinks } from '@/constants';
+import AuthProvider from './AuthProvider';
 
 const Navbar = () => {
+
+    const session = {};
+
     return (
         <nav className='flexBetween navbar'>
             <div className='flex-1 flexStart gap-10'>
@@ -15,6 +20,21 @@ const Navbar = () => {
 
                     </Image>
                 </Link>
+
+                <ul className='xl:flex hidden text-small gap-7'>
+                    {NavLinks.map((link) =>
+                        <Link href={link.href} key={link.key}>
+                            {link.text}
+                        </Link>)}
+                </ul>
+            </div>
+
+            <div className='flexCenter gap-4'>
+                {session ? <>
+                    User Photo
+
+                    <Link href='/create-project'>Share work</Link>                </> :
+                    <AuthProvider />}
             </div>
         </nav>
     );
